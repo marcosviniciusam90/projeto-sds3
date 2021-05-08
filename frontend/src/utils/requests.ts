6ce842cb9f04f4d9285acaa340028b8e2f0a4c10
Marcos Mendonça <marcos.mendonca@matera.com>
2021-05-08 08:02:08 -0300

    Env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,8 @@
-export const BASE_URL = 'http://localhost:8080';
+//se existir a variável de ambiente definida usa o valor, caso contrário usa 'http://localhost:8080'
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+
+/* 
+Variáveis de ambiente com prefixo REACT_APP_ é padrão do Netlify, 
+uma vez que tenha esse prefixo vai ser processada pelo browser e o
+JavaScript terá acesso
+*/
